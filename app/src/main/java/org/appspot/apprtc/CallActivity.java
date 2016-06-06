5d144c575612e16464825b5faf08a8cc2ba316ce
ecmedina <ecmedina@vectorsf.com>
2016-06-06 10:12:06 +0200

    Initialize gitflow

--- app/src/main/java/org/appspot/apprtc/CallActivity.java
+++ app/src/main/java/org/appspot/apprtc/CallActivity.java
@@ -40,13 +40,8 @@ import org.webrtc.StatsReport;
 import org.webrtc.SurfaceViewRenderer;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.IntBuffer;
 import java.util.Date;
-
-import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Activity for peer connection call setup, call waiting
@@ -461,7 +456,7 @@ public class CallActivity extends Activity
         finish();
     }
 
-    private void screenShot1() {
+    private void screenShot() {
         Date now = new Date();
         android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
         String nombre = "vectormobile";
@@ -489,52 +484,6 @@ public class CallActivity extends Activity
         }
     }
 
-    public static Bitmap SavePixels(int x, int y, int w, int h, GL10 gl) {
-        int b[] = new int[w * h];
-        int bt[] = new int[w * h];
-        IntBuffer ib = IntBuffer.wrap(b);
-        ib.position(0);
-        gl.glReadPixels(x, y, w, h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
-
-    /*  remember, that OpenGL bitmap is incompatible with
-        Android bitmap and so, some correction need.
-     */
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                int pix = b[i * w + j];
-                int pb = (pix >> 16) & 0xff;
-                int pr = (pix << 16) & 0x00ff0000;
-                int pix1 = (pix & 0xff00ff00) | pr | pb;
-                bt[(h - i - 1) * w + j] = pix1;
-            }
-        }
-        Bitmap sb = Bitmap.createBitmap(w, h,
-                Bitmap.Config.RGB_565);
-        return sb;
-    }
-
-    public static void screenShot(int x, int y, int w, int h, String name, GL10 gl) {
-        Bitmap bmp = SavePixels(x, y, w, h, gl);
-        try {
-            FileOutputStream fos = new FileOutputStream("/sdcard/my_app/" + name);
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
-            try {
-                fos.flush();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            try {
-                fos.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
 
     private void openScreenshot(File imageFile) {
         Intent intent = new Intent();
